9ddcffe835f63a55cc9f39b71637e97387400c98
naoya <n.fuppy@gmail.com>
2017-10-07 13:01:06 +0900

    p131,二分探索

---
+++ aggressiv_cows.cpp
@@ -0,0 +1,159 @@
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+using namespace std;
+
+
+#define DEBUG(x) cout<<#x<<": "<<x<<endl;
+#define DEBUG_VEC(v) cout<<#v<<":";for(int i=0;i<v.size();i++) cout<<" "<<v[i]; cout<<endl
+
+typedef long long ll;
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector< vector<int> >
+#define vll vector< vector<ll> >
+#define vs vector<string>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pis pair<int,string>
+#define psi pair<string,int>
+const int inf = 1000000001;
+const ll INF = 1e16;
+#define MOD 1000000007
+#define mod 1000000009
+#define pi 3.14159265358979323846
+#define Sp(p) cout<<setprecision(15)<<fixed<<p<<endl;
+int dx[4] = { 1,0,-1,0 }, dy[4] = { 0,1,0,-1 };
+int dx2[8] = { 1,1,0,-1,-1,-1,0,1 }, dy2[8] = { 0,1,1,1,0,-1,-1,-1 };
+#define N 100010
+
+int n, c;
+vector<ll> x;
+
+bool is_able(ll d) {
+	int i;
+	ll now = x[0];
+	ll cnt = 1;
+	for (i = 1; i < n; i++) {
+		if (x[i] - now >= d) {
+			now = x[i];
+			cnt++;
+		}
+	}
+	if (cnt >= c) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+ll _search(ll left, ll right) {
+	while (left + 1 < right) {
+		ll mid = (left + right) / 2;
+		if (is_able(mid)) {
+			left = mid;
+		}
+		else {
+			right = mid;
+		}
+	}
+	return left;
+}
+
+int main() {
+	int i, j;
+	cin >> n >> c;
+	for (i = 0; i < n; i++) {
+		ll j;
+		cin >> j;
+		x.push_back(j);
+	}
+	sort(x.begin(), x.end());
+	ll left = 0;
+	ll right = x[n - 1];
+	ll ans = _search(left, right + 1);
+	cout << ans << endl;
+
+}
