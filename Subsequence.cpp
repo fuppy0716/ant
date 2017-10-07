552fda97f0d5a0bf41afd9e018fcff02be1f868f
naoya <n.fuppy@gmail.com>
2017-10-07 13:36:33 +0900

    p135,しゃくとり法

---
+++ Subsequence.cpp
@@ -0,0 +1,137 @@
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+using namespace std;
+
+
+#define DEBUG(x) cout<<#x<<": "<<x<<endl;
+#define DEBUG_VEC(v) cout<<#v<<":";for(int i=0;i<v.size();i++) cout<<" "<<v[i]; cout<<endl
+
+typedef long long ll;
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector< vector<int> >
+#define vll vector< vector<ll> >
+#define vs vector<string>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pis pair<int,string>
+#define psi pair<string,int>
+const int inf = 1000000001;
+const ll INF = 1e16;
+#define MOD 1000000007
+#define mod 1000000009
+#define pi 3.14159265358979323846
+#define Sp(p) cout<<setprecision(15)<<fixed<<p<<endl;
+int dx[4] = { 1,0,-1,0 }, dy[4] = { 0,1,0,-1 };
+int dx2[8] = { 1,1,0,-1,-1,-1,0,1 }, dy2[8] = { 0,1,1,1,0,-1,-1,-1 };
+int main() {
+	int q;
+	cin >> q;
+	int i0;
+	for (i0 = 0; i0 < q; i0++) {
+		int n, s, i, j;
+		cin >> n >> s;
+		vi a(n);
+		for (i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+		ll right = 1, left = 0, sum = a[0], ans = INF;
+		while (1) {
+			while (right < n && sum < s) {
+				right++;
+				sum += a[right - 1];
+			}
+			if (sum < s) {
+				break;
+			}
+			ans = min(ans, right - left);
+			sum -= a[left++];
+		}
+		if (ans == INF) {
+			ans = 0;
+		}
+		cout << ans << endl;
+	}
+}
